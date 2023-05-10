e8489df756d66661edf84c4cd555ec163474812d
“jmfonsecap” <jmfonsecap@eafit.edu.co>
2023-05-10 06:51:52 -0500

    Ultimo Tutorial T-T

--- ClaseReto1/laravelcourse/app/Http/Controllers/HomeController.php
+++ ClaseReto1/laravelcourse/app/Http/Controllers/HomeController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\View\View;
 
 class HomeController extends Controller
+
 {
     public function index(): View
+    
     {
-        return view('home.index');
+        return view('home.index') ;
+    
+    
     }
 }
